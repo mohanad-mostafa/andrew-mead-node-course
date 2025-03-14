47b5daaeb9fc6c8474087cb80bf6126bf714d8fd
mohanad-mostafa <mohannad.mostafa98@gmail.com>
2025-03-14 20:05:20 +0200

    importing files

---
+++ notes-app/app.js
@@ -0,0 +1,13 @@
+// tutorial 1
+// const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'My name is Andrew.');
+
+// fs.appendFileSync('notes.txt', ' I live in Philadelphia.');
+// ----------------------------------------------------------------------------
+
+// tutorial 2
+const getNotes = require('./utils.js');
+console.log(getNotes());
+
+// ----------------------------------------------------------------------------
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js is running');
+
+const sum = (a, b) => a + b;
+
+const myNotes = function() {
+    return 'Your notes...';
+}
+
+module.exports = myNotes;
